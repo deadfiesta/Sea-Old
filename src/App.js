50218efd4a68a0e5c033ab4dfff1f7b2cce51b17
deadfiesta <61130146+deadfiesta@users.noreply.github.com>
2021-11-08 00:08:07 +0800

    New AppRouter Component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import './scss/main.scss'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Redirect } from 'react-router';
-import { brutalist, flat, glassmorphism, monochromatic, neumorphism, threed } from './Components/Data';
-import Menu from './Components/Menu';
-import Flat from './Components/Flat';
+import AppRouter from './Components/AppRouter';
 
 
 function App() {
   return (
     <div className="App">
-      <Router basename={"/trends"}>
-        <Menu />
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/flat-design"/>
-          </Route>
-          <Route path="/no-css">
-            <Flat data={flat}/>
-          </Route>
-          <Route path="/flat-design">
-            <Flat data={flat}/>
-          </Route>
-          <Route path="/glassmorphism">
-            <Flat data={glassmorphism}/>
-          </Route>
-          <Route path="/neumorphism">
-            <Flat data={neumorphism}/>
-          </Route>
-          <Route path="/monochromatic">
-            <Flat data={monochromatic}/>
-          </Route>
-          <Route path="/brutalist">
-            <Flat data={brutalist}/>
-          </Route>
-          <Route path="/threed">
-            <Flat data={threed}/>
-          </Route>
-        </Switch>
-      </Router>
+      <AppRouter />
     </div>
   );
 }
---
+++ src/Components/AppRouter.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { brutalist, flat, glassmorphism, monochromatic, neumorphism, threed } from './Data';
+import Menu from './Menu';
+import Flat from './Flat';
+
+const AppRouter = () => {
+    return (
+        <Router basename={"/trends"}>
+            <Menu />
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/flat-design" />
+                </Route>
+                <Route path="/no-css">
+                    <Flat data={flat} />
+                </Route>
+                <Route path="/flat-design">
+                    <Flat data={flat} />
+                </Route>
+                <Route path="/glassmorphism">
+                    <Flat data={glassmorphism} />
+                </Route>
+                <Route path="/neumorphism">
+                    <Flat data={neumorphism} />
+                </Route>
+                <Route path="/monochromatic">
+                    <Flat data={monochromatic} />
+                </Route>
+                <Route path="/brutalist">
+                    <Flat data={brutalist} />
+                </Route>
+                <Route path="/threed">
+                    <Flat data={threed} />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter
