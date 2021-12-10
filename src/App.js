77c8e5445c663ce1f6b03232f7ff8919b5b610c1
deadfiesta <61130146+deadfiesta@users.noreply.github.com>
2021-12-10 20:48:27 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
+import { animated, useSpring } from 'react-spring'
+import Toast from "./Components/Toast";
 
 function App() {
+  const [toasts, setToasts] = useState([]);
+  const [message, setMessage] = useState("")
+  const count = useRef(0)
+  const container = useRef()
+  const [move, api] = useSpring(() => ({ y: 0 }))
+
+  const updateToast = (id, duration, height) => {
+    setTimeout(()=> {
+      api.start({ y: container.current.offsetHeight - height, config: {duration: 0} })
+    }, duration)
+    setTimeout(() => {
+      let go = document.getElementById(id)
+      go !== null && go.remove()
+    }, duration);
+  };
+
+  const pushToast = () => {
+    let id = count.current;
+    count.current++;
+
+    const duration = (length) => {
+      let cal = length * 150;
+      if (cal < 3500) return 3500;
+      else if (cal > 10000) return 10000;
+      else return cal;
+    };
+
+    setToasts([
+      ...toasts,
+      <Toast key={id} id={`t${id}`} message={message} duration={duration(message.length)} update={updateToast} />
+    ])
+  }
+
+  useEffect(() => {
+    api.start({ y: container.current.offsetHeight, config: { tension: 170, friction: 26 } })
+    console.log(container)
+  }, [toasts])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <label htmlFor="toast-message" name="toast"></label>
+      <input
+        onChange={(e) => setMessage(e.target.value)}
+        type="text"
+        placeholder="Enter your message"
+        name="toast"
+        id="toast-message"
+      />
+      <button onClick={pushToast}>push</button>
+      <div className="toast-wrapper">
+        <animated.div style={move} ref={container} className="toast-container" >
+          {toasts}
+        </animated.div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Toast.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+import {useSpring, animated } from 'react-spring'
+
+const Toast = ({ message, id, duration, update }) => {
+
+    const toasty = useRef()
+    
+    const [entry, api] = useSpring(()=> ({ opacity: 0, y: -10, config: { tension: 200 } }))
+
+    
+
+    useEffect(()=> {
+        api.start({ opacity: 1, y: 0 })
+        setTimeout(()=> {
+            api.start({ opacity: 0, y: 10 })
+        }, duration - 300)
+    })
+
+    useEffect(()=> {
+        const height = toasty.current.offsetHeight
+        update(id, duration, height)
+    })
+
+    return (
+        <animated.div ref={toasty} style={entry} id={id}>
+            {message}
+        </animated.div>
+    );
+};
+
+export default Toast;
